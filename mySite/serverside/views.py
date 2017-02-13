676e264959a7830ae773865944b31c57e4ecaaee
Jonathan Riordan <c13432152@mydit.ie>
2017-02-13 13:27:08 +0000

    Json

--- mySite/serverside/views.py
+++ mySite/serverside/views.py
@@ -13,7 +13,7 @@ from django.core import serializers
 # Convert the survey data to json
 def surveyData(request):
 	survey = serializers.serialize('json', Post.objects.all())
-	return HttpResponse(survey)
+	return HttpResponse('{ "data": '+survey+'}')
 
 # Convert the question data to json
 def questionData(request):
