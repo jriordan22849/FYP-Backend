5cbf9f8546ccbd06e0f49714ab3255b733aafec1
Jonathan Riordan <c13432152@mydit.ie>
2017-03-05 18:01:02 +0000

    Added the function to update the counter when a survey is completed

--- mySite/posts/models.py
+++ mySite/posts/models.py
@@ -40,4 +40,3 @@ class Answers(models.Model):
 
 
 
-
--- mySite/serverside/views.py
+++ mySite/serverside/views.py
@@ -11,6 +11,7 @@ from django.http import HttpResponse
 from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
 
+
 # Convert the survey data to json
 def surveyData(request):
 	survey = serializers.serialize('json', Post.objects.all())
@@ -26,10 +27,32 @@ def questionData(request):
 def answerData(request):
 	answers = serializers.serialize('json', Answers.objects.all())
 	return HttpResponse('{ "data": '+answers+'}')
-
+	
 @csrf_exempt 
 def dataPost(request):
-    if request.method == "POST":
-        received_json_data=json.loads(request.body)
-        print(received_json_data)
-        return HttpResponse({'received data': request})
+	survey = Post.objects.all()
+	increment = 1
+	if request.method == "POST":
+		#received_json_data = json.loads(request.body)
+		received_json_data = json.dumps(json.loads(request.body))
+		splitJson = ""
+		splitJson = received_json_data.split(",")
+		
+		#remove characters from the string and check to see if the survey title exists, if so, update the times completed by plus one
+		splitJson[0] = splitJson[0].replace("[","")
+		splitJson[0] = splitJson[0].replace('"',"")
+		print("Survey title: " + splitJson[0])
+		
+	    # increment the numbe rof times competed by one if the survey title exists. 
+	    # Update the value in the Post model.
+		if Post.objects.filter(title=splitJson[0]).exists():
+			print 'Survey title exist'
+			survey = Post.objects.get(title = splitJson[0])
+			survey.numOfTimesCompleted = survey.numOfTimesCompleted + increment
+			survey.save()
+
+		
+
+			
+		#print(received_json_data)
+	return HttpResponse({'received data': request})
